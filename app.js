9ca7c42630336c3b65241b0da88f44185c953f0a
Jami Jordan <33480672+JamiJordan25@users.noreply.github.com>
2023-01-23 21:52:05 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,168 @@
+new hoverEffect({
+    parent: document.querySelector('.app'),
+    intensity: 0.1,
+    image1: './image1.jpg',
+    image2: './image2.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app1'),
+    intensity: 0.1,
+    image1: './image1.jpg',
+    image2: './image2.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app2'),
+    intensity: 0.1,
+    image1: './image3.jpg',
+    image2: './image4.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app3'),
+    intensity: 0.1,
+    image1: './image5.jpg',
+    image2: './image6.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app4'),
+    intensity: 0.1,
+    image1: './image7.jpg',
+    image2: './image8.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app5'),
+    intensity: 0.1,
+    image1: './image9.jpg',
+    image2: './image10.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app6'),
+    intensity: 0.1,
+    image1: './image11.jpg',
+    image2: './image12.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app7'),
+    intensity: 0.1,
+    image1: './image13.jpg',
+    image2: './image14.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app8'),
+    intensity: 0.1,
+    image1: './image15.jpg',
+    image2: './image16.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app9'),
+    intensity: 0.1,
+    image1: './image17.jpg',
+    image2: './image18.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app10'),
+    intensity: 0.1,
+    image1: './image19.jpg',
+    image2: './image20.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app11'),
+    intensity: 0.1,
+    image1: './image21.jpg',
+    image2: './image22.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app12'),
+    intensity: 0.1,
+    image1: './image23.jpg',
+    image2: './image24.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app13'),
+    intensity: 0.1,
+    image1: './image25.jpg',
+    image2: './image26.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app14'),
+    intensity: 0.1,
+    image1: './image27.jpg',
+    image2: './image28.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app15'),
+    intensity: 0.1,
+    image1: './image29.jpg',
+    image2: './image30.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app16'),
+    intensity: 0.1,
+    image1: './image31.jpg',
+    image2: './image32.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app17'),
+    intensity: 0.1,
+    image1: './image33.jpg',
+    image2: './image34.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app18'),
+    intensity: 0.1,
+    image1: './image35.jpg',
+    image2: './image36.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app19'),
+    intensity: 0.1,
+    image1: './image37.jpg',
+    image2: './image38.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.app20'),
+    intensity: 0.1,
+    image1: './image39.jpg',
+    image2: './image40.jpg',
+    angle: Math.PI / 8,
+    displacementImage: './heightMap.png'
+});
